773138824fa452a2f08c0903406774c503ce45ca
Muhammad Idrus <muhammadidrus919@gmail.com>
2023-07-13 11:05:12 +0700

    [muhammad idrus] setup theme

---
+++ lib/constans/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+final themeData = ThemeData(
+  primaryColor: primaryColor,
+  fontFamily: 'Poppins',
+);
+
+const primaryColor = Color(0XFFdeb522);
+const secondaryColor = Color(0XFF128bb5);
+
+const neutralColor = Color(0XFF0c0b00);
+const neutral9Color = Color(0XFF454A4D);
+const neutral8Color = Color(0XFF73777A);
+const neutral7Color = Color(0XFFA2A5A6);
+const neutral6Color = Color(0XFFB9BBBC);
+const neutral5Color = Color(0XFFD0D2D3);
+const neutral4Color = Color(0XFFE8E8E9);
+const neutral3Color = Color(0XFFF3F4F4);
+const neutral2Color = Color(0XFFFAFAFB);
+const neutral1Color = Color(0XFFFFFFFF);
