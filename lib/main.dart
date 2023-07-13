004271222756a59d851cc12fb02cfbba6d97b1ea
Muhammad Idrus <muhammadidrus919@gmail.com>
2023-07-13 11:05:48 +0700

    implement theme & route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:get/route_manager.dart';
+import 'package:get/get.dart';
+import 'package:tlab_test/binding.dart';
+import 'package:tlab_test/constans/theme.dart';
+import 'package:tlab_test/route.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,6 +16,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'IMDB Popular Films',
+      theme: themeData,
+      initialRoute: myRoute,
+      initialBinding: MyBinding(),
+      getPages: MyRoutes.routes,
       builder: (context, child) {
         return MediaQuery(
           data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
